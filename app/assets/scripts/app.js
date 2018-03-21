6457ff0aa6b8929171c841133de902c8c374ceb2
Bree <bree@Pink.local>
2018-03-21 15:58:41 -0700

    reveal on scroll for feature items and testestimonials section

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,4 @@
-//mobile menu
+//MOBILE MENU
 //ES6
 // class MobileMenu {
 // 	constructor() {
@@ -40,3 +40,64 @@ function MobileMenu() {
 }
 
 var mobileMenu = new MobileMenu();
+
+//REVEAL-ON-SCROLL
+
+//ES6
+// class RevealOnScroll {
+// 	constructor(els, offset) {
+// 		this.itemsToReveal = els;
+// 		this.offsetPercentage = offset;
+// 		this.hideInititally();
+// 		this.createWaypoints();
+// 	}
+
+// 	hideInititally() {
+// 		this.itemsToReveal.addClass("reveal-item");
+// 	}
+
+// 	createWaypoints() {
+// 		var that = this;  //below this code, "this" points to different locations so delcate it first
+// 		this.itemsToReveal.each(function(){ 
+// 			var currentItem = this;
+// 			new Waypoint({
+// 				element: currentItem,
+// 				handler: function() {
+// 					$(currentItem).addClass("reveal-item--is-visible");
+// 				},
+// 				offset: that.offsetPercentage
+// 			});
+// 		});
+// 	}
+// }
+
+//ES5
+function RevealOnScroll(els, offset) {
+	this.itemsToReveal = els;
+	this.hideInititally = function() {
+		this.itemsToReveal.addClass("reveal-item");
+	}
+	this.createWaypoints = function() {
+		this.itemsToReveal.each(function(){ 
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,
+				handler: function() {
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: offset
+			});
+		});
+	}
+
+	this.hideInititally();
+	this.createWaypoints();
+
+}
+
+var revealOnScrollFeatureItems = new RevealOnScroll($(".feature-item"), "80%");
+var revealOnScrollTestimonials = new RevealOnScroll($(".testimonial"), "60%");
+
+
+
+
